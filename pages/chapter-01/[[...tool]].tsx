9f0614a39e34b2c32d7edfbc38bab50aaa852731
Cody Olsen <stipsan@gmail.com>
2022-06-13 23:42:55 +0200

    add it to the list!

--- pages/chapter-01/[[...tool]].tsx
+++ pages/chapter-01/[[...tool]].tsx
@@ -1,112 +1,11 @@
-import { config as blogConfig } from 'components/studios/blog'
 import StudioPage from 'components/StudioPage'
-import { useMemo } from 'react'
-import { Studio, createConfig } from 'sanity'
-
-import { useBasePath } from 'hooks'
-import {
-  DefaultDocumentNodeResolver,
-  deskTool,
-  View,
-  ViewBuilder,
-} from 'sanity/desk'
-import HeroPost from 'components/blog/HeroPost'
-import BlogPreviewWrapper from 'components/studios/chapter-01/BlogPreviewWrapper'
-import PostPreview from 'components/blog/PostPreview'
-import MoreStories from 'components/blog/MoreStories'
-import PostHeader from 'components/blog/PostHeader'
-import { cover } from 'polished'
-import PostBody from 'components/blog/PostBody'
-
-// @ts-expect-error
-const MorePreview = (props) => {
-  return <MoreStories posts={[props]} />
-}
-
-const DetailPreview = ({
-  // @ts-expect-error
-  title,
-  // @ts-expect-error
-  date,
-  // @ts-expect-error
-  slug,
-  // @ts-expect-error
-  excerpt,
-  // @ts-expect-error
-  coverImage,
-  // @ts-expect-error
-  author,
-  // @ts-expect-error
-  content,
-}) => {
-  return (
-    <>
-      <PostHeader
-        author={author}
-        coverImage={coverImage}
-        title={title}
-        date={date}
-      />
-      <PostBody content={content} />
-    </>
-  )
-}
-
-const defaultDocumentNode: DefaultDocumentNodeResolver = (
-  S,
-  { schemaType }
-) => {
-  const views: (View | ViewBuilder)[] = []
-
-  views.push(S.view.form())
-
-  if (schemaType === 'post') {
-    // views.push(S.view.component(({ document: { displayed: { title, date, slug, excerpt}} }) =>
-    //  <HeroPost title={title} date={date} excerpt={excerpt} slug={slug.current} />
-    // ).id('hero-preview').title('Hero'))
-    views.push(
-      S.view
-        .component(({ document }) => (
-          <BlogPreviewWrapper document={document} Component={HeroPost} />
-        ))
-        .id('hero-preview')
-        .title('Hero')
-    )
-    views.push(
-      S.view
-        .component(({ document }) => (
-          <BlogPreviewWrapper document={document} Component={MorePreview} />
-        ))
-        .id('more-preview')
-        .title('More')
-    )
-    views.push(
-      S.view
-        .component(({ document }) => (
-          <BlogPreviewWrapper document={document} Component={DetailPreview} />
-        ))
-        .id('detail-preview')
-        .title('Detail')
-    )
-  }
-
-  return S.document().views(views)
-}
+import { Studio } from 'sanity'
+import config from 'sanity.config'
 
 export default function StudioRoute() {
-  const basePath = useBasePath()
-  const config = useMemo(
-    () =>
-      createConfig({
-        ...blogConfig,
-        basePath,
-        plugins: [deskTool({ defaultDocumentNode })],
-      }),
-    [basePath]
-  )
   return (
     <StudioPage>
-      <Studio config={config} />
+      <Studio config={config} unstable_noAuthBoundary />
     </StudioPage>
   )
 }
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,12 @@
 import { createConfig } from 'sanity'
 import { config as blogConfig } from 'components/studios/blog'
+import { config as blogWithPreviews } from 'components/studios/chapter-01'
 import { config as wordpressConfig } from 'components/studios/wordpress'
 import { config as themerConfig } from 'components/studios/themer'
 
-export default createConfig([blogConfig, wordpressConfig, themerConfig])
+export default createConfig([
+  blogConfig,
+  blogWithPreviews,
+  wordpressConfig,
+  themerConfig,
+])
