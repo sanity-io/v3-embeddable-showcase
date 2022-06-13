d713ba0bfafbe52aea1dca61722f0a865b5e7768
Cody Olsen <stipsan@gmail.com>
2022-06-13 19:21:13 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: true,
+    // ignoreBuildErrors: true,
   },
 }
 
