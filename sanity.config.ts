0129b895295471a667f9b5d17e10be3a43d4abe7
Cody Olsen <stipsan@gmail.com>
2022-06-14 14:54:19 +0200

    fix: upgrading to dev-preview

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,7 +3,6 @@ import { config as blogConfig } from 'components/studios/blog'
 import { config as proBlogConfig } from 'components/studios/blog-pro'
 import { config as maxProBlogConfig } from 'components/studios/blog-pro-max'
 import { config as themerConfig } from 'components/studios/themer'
-import { projectId, dataset } from 'hooks/useSanityClient'
 
 const workspaces = createConfig([
   // manage/blog
@@ -14,14 +13,6 @@ const workspaces = createConfig([
   maxProBlogConfig,
   // manage/themer
   themerConfig,
-  // May not work as it uses different routing
-  {
-    name: 'studio-on-demand',
-    title: 'V3 + ESM = <3',
-    basePath: '/__native-esm__/desk.html',
-    dataset,
-    projectId,
-  },
 ])
 
 export default workspaces
