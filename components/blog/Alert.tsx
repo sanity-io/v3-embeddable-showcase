ee8ccf42c227957f5ccb4b6e813651a371983112
Cody Olsen <stipsan@gmail.com>
2022-06-14 11:25:00 +0200

    slightly better live preview

--- components/blog/Alert.tsx
+++ components/blog/Alert.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link'
 import Container from './Container'
 import cn from 'classnames'
+import Head from 'next/head'
+import favicon from 'public/sanity.svg'
+import previewModeFavicon from 'public/preview-mode.svg'
 
 export default function Alert({ preview }: { preview?: boolean }) {
   return (
@@ -12,18 +15,26 @@ export default function Alert({ preview }: { preview?: boolean }) {
     >
       <Container>
         {preview ? (
-          <div className="py-2 text-sm text-center">
-            <>
-              This page is a preview.{' '}
-              <Link href="/api/exit-preview"><a
-                
-                className="underline transition-colors duration-200 hover:text-cyan"
-              >
-                Click here
-              </a></Link>{' '}
-              to exit preview mode.
-            </>
-          </div>
+          <>
+            <Head>
+              <link
+                rel="icon"
+                href={preview ? previewModeFavicon.src : favicon.src}
+                type="image/svg+xml"
+              />
+            </Head>
+            <div hidden className="py-2 text-center text-sm">
+              <>
+                This page is a preview.{' '}
+                <Link href="/api/exit-preview">
+                  <a className="underline transition-colors duration-200 hover:text-cyan">
+                    Click here
+                  </a>
+                </Link>{' '}
+                to exit preview mode.
+              </>
+            </div>
+          </>
         ) : (
           <></>
         )}
