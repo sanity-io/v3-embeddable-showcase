4bb6a9d1661d9387f4ede1696459e02c98ee604f
Cody Olsen <stipsan@gmail.com>
2022-06-14 16:04:25 +0200

    Update [[...workspace]].tsx

--- pages/manage/[[...workspace]].tsx
+++ pages/manage/[[...workspace]].tsx
@@ -1,13 +1,10 @@
+import Singularity from 'components/Singularity'
+import { useEffect, useState } from 'react'
 import StudioPage from 'components/StudioPage'
 import { config as themerConfig } from 'components/studios/themer'
 import { createClient, workspacesQuery } from 'hooks/useSanityClient'
 import { useWorkspacesFromThemer } from 'hooks/useSanityStudio'
 import { Studio, type WorkspaceOptions } from 'sanity'
-import config from 'sanity.config'
-
-import { createStudioTheme, useTonesFromPreset } from 'hooks'
-import { useMemo } from 'react'
-import workspaces from 'sanity.config'
 
 export interface Props {
   workspaces: WorkspaceOptions[]
@@ -15,22 +12,31 @@ export interface Props {
 
 // WTODO: query workspace theme data from themer
 export default function ManageWorkspace(props: Props) {
-  console.log('ManageWorkspace!!', { props })
-  /*
-  const preset = useTonesFromPreset({ preset: 'imagepalette' })
-  const fallbackTheme = useCustomStudioTheme({ config: preset })
-  const theme = props.theme || fallbackTheme
-  const appliedTheme = useMemo(
-    () => workspaces.map((workspace) => ({ ...workspace, theme })),
-    [theme]
-  )
-  // */
+  const [singularity, setSingularity] = useState(false)
+  // eslint-disable-next-line react-hooks/exhaustive-deps,react-hooks/rules-of-hooks
+  useEffect(() => {
+    const handler = () => {
+      const maybeSingularity = document.querySelectorAll(
+        '[data-studio-preview]'
+      )
+      if (maybeSingularity.length > 3 && !singularity) {
+        setSingularity(true)
+      }
+    }
+    handler()
+    window.addEventListener('focus', handler)
+    return () => window.removeEventListener('focus', handler)
+  }, [singularity])
+
   const [configMergedWithThemer] = useWorkspacesFromThemer()
 
   return (
-    <StudioPage>
-      <Studio config={configMergedWithThemer} unstable_noAuthBoundary />
-    </StudioPage>
+    <>
+      <StudioPage>
+        <Studio config={configMergedWithThemer} unstable_noAuthBoundary />
+      </StudioPage>
+      <Singularity singularity={singularity} />
+    </>
   )
 }
 
@@ -38,7 +44,6 @@ export async function getServerProps(): Promise<{ props: Props }> {
   const { dataset } = themerConfig
   const client = createClient().withConfig({ dataset })
   const data = await client.fetch(workspacesQuery)
-  console.log('getServerProps', data)
   return {
     props: {
       workspaces: Array.isArray(data) ? data : [],
